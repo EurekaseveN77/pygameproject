31e9f913aea8e5e5375ae71f05daa1137e9c780c
Echo9erlogos <andres00espar@gmail.com>
2022-04-14 11:40:56 -0700

    support.py change line 9 to f string

--- pygameproject-James-Branch/pygameproject-James-Branch/support.py
+++ pygameproject-James-Branch/pygameproject-James-Branch/support.py
@@ -6,7 +6,7 @@ def import_folder(path):
 
     for _,_,img_files in walk(path):
         for image in img_files:
-            full_path = path + '/' + image
+            full_path = f'{path}/{image}'
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
 
