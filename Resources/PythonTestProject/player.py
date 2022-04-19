cc586005186f03fa2728e83afbd1561d8906424d
CyberBinary <42885189+CyberBinary@users.noreply.github.com>
2022-04-18 21:48:47 -0700

    Animation added
    Fixed sprite display issue and added the function to get player status and animate player.

--- Resources/PythonTestProject/player.py
+++ Resources/PythonTestProject/player.py
@@ -16,13 +16,26 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+        # player status
+        self.status = 'idle'
+
     def import_character_assets(self):
-        character_path = '../graphics/character/'
+        character_path = 'Resources/graphics/character/'
         self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
         
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations[self.get_status()]
+
+        #loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -37,6 +50,17 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_SPACE]:
             self.jump()
 
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
+
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
@@ -44,6 +68,7 @@ class Player(pygame.sprite.Sprite):
     def jump(self):
         self.direction.y = self.jump_speed
         
-
     def update(self):
         self.get_input()
+        self.get_status()
+        self.animate()
--- Resources/PythonTestProject/support.py
+++ Resources/PythonTestProject/support.py
@@ -3,8 +3,7 @@ import pygame
 
 def import_folder(path):
     surface_list = []
-
-    for _,__,img_files in walk(path):
+    for __,___,img_files in walk(path):
         for image in img_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
