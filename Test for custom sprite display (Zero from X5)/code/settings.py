77259087d4490404e1d9f4f1ed91e4f75c97f214
EurekaseveN77 <43527523+EurekaseveN77@users.noreply.github.com>
2022-04-23 20:18:47 -0700

    Test for Custom asset used for player character
    I added sprites from a character that I cropped the sprites from a sprite sheet and used photoshop to add transparency.

---
+++ Test for custom sprite display (Zero from X5)/code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                   X        ',
+'                   X        ',
+'                   X        ',
+' XX    XXX         X  XX    ',
+' XX P              X        ',
+' XXXX              X     XX ',
+' XXXX       XX     X        ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXXXXXX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
