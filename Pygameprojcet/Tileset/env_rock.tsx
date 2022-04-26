df3e3b5f098e0dda2e515c76bce042f1b4f0c968
Andres <andres00espar@gmail.com>
2022-04-26 11:44:42 -0700

    had to remove level made an error

--- Pygameprojcet/Tileset/env_rock.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="env_rock" tilewidth="64" tileheight="64" tilecount="49" columns="7">
- <image source="DarkForest1.2/env_rock.png" width="480" height="480"/>
-</tileset>
--- Pygameprojcet/Tileset/env_trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="env_trees" tilewidth="64" tileheight="64" tilecount="64" columns="8">
- <image source="DarkForest1.2/env_trees.png" width="544" height="544"/>
-</tileset>
