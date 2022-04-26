f88bf98282990901a126de86f667bf0468af2837
Andres <andres00espar@gmail.com>
2022-04-26 12:03:36 -0700

    created new level 0
    its under tutorial code then 2-level

---
+++ Tutorial Code/2-Level/levels/tilesets/env_ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="env_ground" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="../../../../../../../Desktop/DarkForest1.2/env_ground.png" width="480" height="480"/>
+</tileset>
