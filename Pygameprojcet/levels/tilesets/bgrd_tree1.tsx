70b495e803275bf162d6e0571649e7b325df3ced
Andres <andres00espar@gmail.com>
2022-04-25 18:48:30 -0700

    Created another level in 16x16
    The new level is created under the levels folder

---
+++ Pygameprojcet/levels/tilesets/bgrd_tree1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="bgrd_tree1" tilewidth="16" tileheight="16" tilecount="918" columns="51">
+ <image source="Desktop/DarkForest1.2/bgrd_tree1.png" width="816" height="288"/>
+</tileset>
