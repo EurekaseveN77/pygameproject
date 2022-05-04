b3b8b6f0cb7cfd63891d929867e663357237d22e
Andres <andres00espar@gmail.com>
2022-05-04 16:35:38 -0700

    new level

---
+++ Project/Real_Code/levels/tilesets/grass_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="grass_tiles" tilewidth="16" tileheight="16" tilecount="28" columns="14">
+ <image source="../../graphics/decoration/grass/grass_tiles.png" width="238" height="34"/>
+</tileset>
---
+++ Project/Real_Code/levels/tilesets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="terrain_tiles" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="../../graphics/terrain/terrain_tiles.png" width="480" height="480"/>
+</tileset>
