650ca5ba89e73d2bada1a2b9d5330096cff6525a
Andres <andres00espar@gmail.com>
2022-05-03 17:49:54 -0700

    update

--- Tutorial_Code/2-Level/code/game_data.py
+++ Tutorial_Code/2-Level/code/game_data.py
@@ -1,5 +1,4 @@
 level_0 = {
             'terrain': 'Tutorial_Code/2-Level/levels/0/level_0_terrain.csv',
             'grass': 'Tutorial_Code/2-Level/levels/0/level_0_grass.csv',
-            'moon': 'Tutorial_Code/2-Level/levels/0/level_0_moon.csv',
             'bg trees': 'Tutorial_Code/2-Level/levels/0/level_0_bg_trees.csv',}
