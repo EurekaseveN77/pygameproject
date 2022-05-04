3ffdde3a2abfaa3e6b166d7f99f4e6af3f95390a
Andres <andres00espar@gmail.com>
2022-05-03 18:46:17 -0700

    rework test

---
+++ Pygameprojcet/Test_Code/code/game_data.py
@@ -0,0 +1,4 @@
+level_0 = {
+            'terrain': 'Test_Code/levels/0/level_0_terrain.csv',
+            'grass': 'Test_Code/2-Level/levels/0/level_0_grass.csv',
+            'bg trees': 'Test_Code/2-Level/levels/0/level_0_bg_trees.csv',}
---
+++ Pygameprojcet/Test_Code/levels/tilesets/env_rock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="env_rock" tilewidth="16" tileheight="16" tilecount="900" columns="30">
+ <image source="../../../../../../../Desktop/DarkForest1.2/env_rock.png" width="480" height="480"/>
+</tileset>
