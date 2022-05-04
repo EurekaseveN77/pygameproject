14891f40f92b087debc965db4cb8af53aaac177d
Andres <andres00espar@gmail.com>
2022-05-04 15:26:03 -0700

    rewrote

--- Project/Test_Code/code/game_data.py
+++ Project/Test_Code/code/game_data.py
@@ -1,4 +1,4 @@
 level_0 = {
-            'terrain': 'Project/Test_Code/levels/0/level_0_terrian.csv',
+            'terrain': 'Project/Test_Code/levels/0/level_0_terrain.csv',
             'grass': 'Project/Test_Code/levels/0/level_0_grass.csv',
             'bg trees': 'Project/Test_Code/levels/0/level_0_bg_trees.csv',}
