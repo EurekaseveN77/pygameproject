418ec0eba9a49475ec6a227871266c9e8d0a294f
Andres <andres00espar@gmail.com>
2022-05-04 17:19:25 -0700

    level 0 created
    enemies are placed, red constraint blocks, as well the start and finish

---
+++ Project/Real_Code/levels/tilesets/enemy_tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="enemy_tiles" tilewidth="67" tileheight="29" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="67" height="29" source="../../graphics/enemy/run/2.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../../graphics/enemy/red box.png"/>
+ </tile>
+</tileset>
