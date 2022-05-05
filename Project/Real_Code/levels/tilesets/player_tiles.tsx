8e9b560726bd6b55654be6b13a3e7787be5160d2
Andres <andres00espar@gmail.com>
2022-05-04 17:22:33 -0700

    Update player_tiles.tsx

--- Project/Real_Code/levels/tilesets/player_tiles.tsx
+++ Project/Real_Code/levels/tilesets/player_tiles.tsx
@@ -2,9 +2,9 @@
 <tileset version="1.8" tiledversion="1.8.4" name="player_tiles" tilewidth="16" tileheight="16" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="../../../../../../../Desktop/start.png"/>
+  <image width="16" height="16" source="../../graphics/character/end.png"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="../../../../../../../Desktop/end.png"/>
+  <image width="16" height="16" source="../../graphics/character/start.png"/>
  </tile>
 </tileset>
