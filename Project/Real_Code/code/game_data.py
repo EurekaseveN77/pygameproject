0a08401b83eb97c683d19dbcb48d396baa1d8de1
Andres <andres00espar@gmail.com>
2022-05-04 18:04:59 -0700

    created code for main level game data settings

---
+++ Project/Real_Code/code/game_data.py
@@ -0,0 +1,6 @@
+level_0 = {
+            'terrain': 'Project/Real_Code/levels/0/level_0_terrain.csv',
+            'enemies': 'Project/Real_Code/levels/0/level_0_enemies.csv',
+            'constraints': 'Project/Real_Code/levels/0/level_0_constraints.csv',
+            'player': 'Project/Real_Code/levels/0/level_0_player.csv',
+            'grass': 'Project/Real_Code/levels/0/level_0_grass.csv', }
