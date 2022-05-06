86174da7fd5157091d0673857afe93932fda76de
Andres <andres00espar@gmail.com>
2022-05-06 12:52:02 -0700

    added sky

---
+++ Project/Real_Code/code/decoration.py
@@ -0,0 +1,24 @@
+from settings import vertical_tile_number,tile_size, screen_width
+import pygame
+
+class Sky:
+    def __init__(self,horizon):
+       self.top = pygame.image.load('Project/Real_Code/graphics/decoration/sky/sky_top.png').convert()
+       self.bottom = pygame.image.load('Project/Real_Code/graphics/decoration/sky/sky_bottom.png').convert()
+       self.middle = pygame.image.load('Project/Real_Code/graphics/decoration/sky/sky_middle.png').convert()
+       self.horizon = horizon
+
+       #stretch
+       self.top = pygame.transform.scale(self.top,(screen_width,tile_size))
+       self.bottom = pygame.transform.scale(self.bottom,(screen_width,tile_size))
+       self.middle = pygame.transform.scale(self.middle,(screen_width,tile_size))
+    
+    def draw(self,surface):
+        for row in range(vertical_tile_number):
+            y = row * tile_size
+            if row < self.horizon:
+                surface.blit(self.top,(0,y))
+            elif row == self.horizon:
+                surface.blit(self.middle,(0,y))
+            else:
+                surface.blit(self.bottom,(0,y))
--- Project/Real_Code/code/level.py
+++ Project/Real_Code/code/level.py
@@ -3,6 +3,7 @@ from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
 from tiles import Tile, StaticTile
 from enemy import Enemy
+from decoration import Sky
 
 class Level:
     def __init__(self,level_data,surface):
@@ -11,10 +12,10 @@ class Level:
         self.world_shift = 0
 
         #player
-        player_layout = import_csv_layout(level_data['player'])
-        self.player = pygame.sprite.GroupSingle()
-        self.goal = pygame.sprite.GroupSingle()
-        self.player_setup(player_layout)
+        #player_layout = import_csv_layout(level_data['player'])
+        #self.player = pygame.sprite.GroupSingle()
+        #self.goal = pygame.sprite.GroupSingle()
+        #self.player_setup(player_layout)
 
         #terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
@@ -23,6 +24,9 @@ class Level:
         #enemy
         enemy_layout = import_csv_layout(level_data['enemies'])
         self.enemy_sprites = self.create_tile_group(enemy_layout, 'enemies')
+
+        #decoration
+        self.sky = Sky(8)
 
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -39,12 +43,12 @@ class Level:
                         sprite = StaticTile(tile_size,x,y,tile_surface)
                         sprite_group.add(sprite)
                     
-                    if type == 'enemies':
-                        sprite = Enemy(tile_size,x,y)
+                    #if type == 'enemies':
+                        #sprite = Enemy(tile_size,x,y)
 
         return sprite_group
     
-    def player_setup(self,layout):
+    #def player_setup(self,layout):
         for row_index, row in enumerate(layout):
             for col_index,val in enumerate(row):
                 x = col_index * tile_size
@@ -52,19 +56,27 @@ class Level:
                 if val == '0':
                     print('player goes here')
                 if val == '1':
-                    hat_surface = pygame.image.load('Project/Real_Code/graphics/character/hat.png').convert_alpha
-                    sprite = StaticTile(tile_size,x,y,hat_surface)
+                    end_surface = pygame.image.load('Project/Real_Code/graphics/character/end.png').convert_alpha
+                    sprite = StaticTile(tile_size,x,y,end_surface)
                     self.goal.add(sprite)
 
     def run(self):
         #run the entire game / level
+        
+         #decoration
+        self.sky.draw(self.display_surface)
+
+        #terrain
         self.terrain_sprites.update(self.world_shift)
         self.terrain_sprites.draw(self.display_surface)
-        
+
+
         #enemy
         self.enemy_sprites.update(self.world_shift)
         self.enemy_sprites.draw(self.display_surface)
 
         #player sprites
-        self.goal.update(self.world_shift)
-        self.goal.draw(self.display_surface)
+        #self.goal.update(self.world_shift)
+        #self.goal.draw(self.display_surface)
+
+        
