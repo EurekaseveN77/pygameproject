685eaf9587ead3fc4c7fa8d2bf03e742c1ad4946
Andres <andres00espar@gmail.com>
2022-05-06 18:39:35 -0700

    created code

---
+++ Project2/code/game_data.py
@@ -0,0 +1,5 @@
+level_0 = {
+            'terrain': 'Project2/levels/0/level_0_terrain.csv',
+            'enemies': 'Project/levels/0/level_0_enemies.csv',
+            'constraints': 'Project2/levels/0/level_0_constraints.csv',
+            'player': 'Project2/levels/0/level_0_player.csv', }
