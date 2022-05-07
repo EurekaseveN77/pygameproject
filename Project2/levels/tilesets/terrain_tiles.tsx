3dfba0e0a361592e529f23ea55d4890f59d99c2b
Andres <andres00espar@gmail.com>
2022-05-06 18:25:16 -0700

    created new level

---
+++ Project2/levels/tilesets/terrain_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="terrain_tiles" tilewidth="16" tileheight="16" tilecount="280" columns="28">
+ <image source="../../graphics/terrain/terrain_tiles.png" width="448" height="160"/>
+</tileset>
