10f4b936741db5ff3796f7be76285411f8a8a878
Luis20099 <racoon553@gmail.com>
2022-05-08 15:17:50 -0700

    UI
    The ui part. this will show th useres health basr in green and on the left hand side of the screen.

---
+++ UI/UI.py
@@ -0,0 +1,21 @@
+import pygame
+#new testing onne
+
+class UI:
+	def __init__(self,surface):
+
+		# setup 
+		self.display_surface = surface 
+
+		# health 
+		self.health_bar = pygame.image.load('../UI/Health_bar.png').convert_alpha()
+		self.health_bar_topleft = (60,50)
+		self.bar_max_width = 150
+		self.bar_height = 5
+
+	def show_health(self,current,full):
+		self.display_surface.blit(self.health_bar,(20,10))
+		current_health_ratio = current / full
+		current_bar_width = self.bar_max_width * current_health_ratio
+		health_bar_rect = pygame.Rect(self.health_bar_topleft,(current_bar_width,self.bar_height))
+		pygame.draw.rect(self.display_surface,'#048703',health_bar_rect)
