f1dd5ac0455a30a9e242be7b03e55067c81c3ab4
Andres <andres00espar@gmail.com>
2022-05-07 17:56:39 -0700

    added enemy class but they don't show up

---
+++ Project2/code/enemy.py
@@ -0,0 +1,7 @@
+import pygame
+from tiles import AnimatedTile
+
+class Enemy(AnimatedTile):
+    def __init__(self, size, x, y,):
+        super().__init__(size, x, y,'Project2/graphics/enemy/run')
+        self.rect.y += size - self.image.get_size()[1]
--- Project2/code/level.py
+++ Project2/code/level.py
@@ -2,6 +2,7 @@ import pygame
 from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
 from tiles import Tile, StaticTile
+from enemy import Enemy
 
 class Level:
     def __init__(self,level_data,surface):
@@ -12,6 +13,10 @@ class Level:
         #terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
+        #enemy
+        enemy_layout = import_csv_layout(level_data['enemies'])
+        self.enemy_sprites = self.create_tile_group(enemy_layout, 'enemies')
     
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -26,11 +31,21 @@ class Level:
                         terrain_tile_list = import_cut_graphics('Project2/graphics/terrain/terrain_tiles.png')
                         tile_surface = terrain_tile_list[int(val)]
                         sprite = StaticTile(tile_size,x,y,tile_surface)
-                        sprite_group.add(sprite)
+                        
+                    if type == 'enemies':
+                        sprite = Enemy(tile_size,x,y)
+                        
+                    sprite_group.add(sprite)
         
         return sprite_group
     
     def run(self):
         #run the entire game / level
+
+        #Terrain
         self.terrain_sprites.update(self.world_shift)
         self.terrain_sprites.draw(self.display_surface)
+
+        #Enemy
+        self.enemy_sprites.update(self.world_shift)
+        self.enemy_sprites.draw(self.display_surface)
