f2c3caa7449206dc50e6e6351713304b6502b5c2
EurekaseveN77 <43527523+EurekaseveN77@users.noreply.github.com>
2022-05-09 18:51:28 -0700

    final version

--- Final Project/Project/code/level.py
+++ Final Project/Project/code/level.py
@@ -1,5 +1,5 @@
 import pygame 
-from support import import_csv_layout, import_cut_graphics, edit_save
+from support import import_csv_layout, import_cut_graphics
 from settings import tile_size, screen_height, screen_width
 from tiles import Tile, StaticTile, Crate, Coin, Palm
 from enemy import Enemy
@@ -10,7 +10,7 @@ from particles import ParticleEffect
 from game_data import levels
 
 class Level:
-	def __init__(self,current_level,surface,create_level,change_coins,change_health):
+	def __init__(self,current_level,surface,create_overworld,change_coins,change_health):
 		# general setup
 		self.display_surface = surface
 		self.world_shift = 0
@@ -21,11 +21,10 @@ class Level:
 		self.stomp_sound = pygame.mixer.Sound('../audio/effects/stomp.wav')
 
 		# overworld connection 
-		#self.create_overworld = create_overworld
-		self.create_level = create_level
+		self.create_overworld = create_overworld
 		self.current_level = current_level
 		level_data = levels[self.current_level]
-		#self.new_max_level = level_data['unlock']
+		self.new_max_level = level_data['unlock']
 
 		# player 
 		player_layout = import_csv_layout(level_data['player'])
@@ -224,13 +223,11 @@ class Level:
 
 	def check_death(self):
 		if self.player.sprite.rect.top > screen_height:
-			self.create_level(self.current_level)
+			self.create_overworld(self.current_level,0)
 			
 	def check_win(self):
 		if pygame.sprite.spritecollide(self.player.sprite,self.goal,False):
-			self.current_level += 1
-			edit_save(self.current_level)
-			self.create_level(self.current_level)
+			self.create_overworld(self.current_level,self.new_max_level)
 			
 	def check_coin_collisions(self):
 		collided_coins = pygame.sprite.spritecollide(self.player.sprite,self.coin_sprites,True)
--- Final Project/Project/code/main.py
+++ Final Project/Project/code/main.py
@@ -3,7 +3,6 @@ from settings import *
 from level import Level
 from overworld import Overworld
 from ui import UI
-from support import read_save, edit_save
 
 class Game:
 	def __init__(self):
@@ -19,28 +18,27 @@ class Game:
 		self.overworld_bg_music = pygame.mixer.Sound('../audio/overworld_music.wav')
 
 		# overworld creation
-		#self.overworld = Overworld(0,self.max_level,screen,self.create_level)
-		#self.status = 'overworld'
-		#self.overworld_bg_music.play(loops = -1)
-		self.create_level(read_save())
+		self.overworld = Overworld(0,self.max_level,screen,self.create_level)
+		self.status = 'overworld'
+		self.overworld_bg_music.play(loops = -1)
+
 		# user interface 
 		self.ui = UI(screen)
 
 
 	def create_level(self,current_level):
-		self.level = Level(current_level,screen,self.create_level,self.change_coins,self.change_health)
+		self.level = Level(current_level,screen,self.create_overworld,self.change_coins,self.change_health)
 		self.status = 'level'
-	#	self.overworld_bg_music.stop()
-		self.level_bg_music.stop()
+		self.overworld_bg_music.stop()
 		self.level_bg_music.play(loops = -1)
 
-	#def create_overworld(self,current_level,new_max_level):
-	#	if new_max_level > self.max_level:
-	#		self.max_level = new_max_level
-	#	self.overworld = Overworld(current_level,self.max_level,screen,self.create_level)
-	#	self.status = 'overworld'
-	#	self.overworld_bg_music.play(loops = -1)
-	#	self.level_bg_music.stop()
+	def create_overworld(self,current_level,new_max_level):
+		if new_max_level > self.max_level:
+			self.max_level = new_max_level
+		self.overworld = Overworld(current_level,self.max_level,screen,self.create_level)
+		self.status = 'overworld'
+		self.overworld_bg_music.play(loops = -1)
+		self.level_bg_music.stop()
 
 	def change_coins(self,amount):
 		self.coins += amount
@@ -61,20 +59,15 @@ class Game:
 			self.overworld = Overworld(0,self.max_level,screen,self.create_level)
 			self.status = 'overworld'
 			self.level_bg_music.stop()
-			self.level_bg_music.play()
-			#self.overworld_bg_music.play(loops = -1)
-	
-	def start_new_game(self):
-		edit_save(0)
-		self.create_level(0)
+			self.overworld_bg_music.play(loops = -1)
 
 	def run(self):
-		#if self.status == 'overworld':
-		#	self.overworld.run()
+		if self.status == 'overworld':
+			self.overworld.run()
 
 		#elif self.status == 'paused':
 			#self.level.run()
-		#else:
+		else:
 			self.level.run()
 			self.ui.show_health(self.cur_health,self.max_health)
 			self.ui.show_coins(self.coins)
@@ -86,22 +79,23 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 game = Game()
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
+def startGame():
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_p:
-				game.get_pause()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_p:
+					game.get_pause()
 
 	
-	screen.fill('grey')
-	game.run()
+		screen.fill('grey')
+		game.run()
 
-	pygame.display.update()
-	clock.tick(60)
+		pygame.display.update()
+		clock.tick(60)
